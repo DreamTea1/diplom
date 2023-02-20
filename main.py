e2afc6503bf07c8effa0784bc3a0c08c527dd6bb
Artem Ershov <artem.ershov.25@yandex.ru>
2023-02-20 13:02:39 +0300

    change get token

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from glob import glob
 
 user_id = input('Введите user_id: ')
 token = input('Введите token с ПОЛИГОНА: ')
-access_token = input('Введите token с ВК!')
+# access_token = input('Введите token с ВК!')
+access_token = ''
 
 count_images = 5
 photo_urls_likes_and_date = vk.get_urls_likes_size_and_dates(access_token, user_id)
